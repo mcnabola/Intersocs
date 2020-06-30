6e6301d072633327a1196cb940a71b4fcaffd789
mcnabola <17226163@studentmail.ul.ie>
2020-06-30 02:12:08 +0100

    add: initial commit of react files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Particles from 'react-tsparticles';
+
+class App extends Component{
+     render(){
+ return (
+ <Particles id="tsparticles" params={{
+  background: {
+    color: {
+      value: "#0d47a1"
+    }
+  },
+  fpsLimit: 60,
+  interactivity: {
+    detectsOn: "canvas",
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push"
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse"
+      },
+      resize: true
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+        speed: 3
+      },
+      push: {
+        quantity: 4
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4
+      }
+    }
+  },
+  particles: {
+    color: {
+      value: "#ffffff"
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1
+    },
+    collisions: {
+      enable: true
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 6,
+      straight: false
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 800
+      },
+      value: 80
+    },
+    opacity: {
+      value: 0.5
+    },
+    shape: {
+      type: "circle"
+    },
+    size: {
+      random: true,
+      value: 5
+    }
+  },
+  detectRetina: true
+}} />
+     );
+  }
 }
 
 export default App;
